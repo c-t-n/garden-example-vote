459607e1b286fca5a8963375ea468741370a5c24
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2019-05-21 10:01:15 +0200

    Replace Cats & Dogs with Tabs & Spaces.

--- vote/src/views/Home.vue
+++ vote/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <Vote optionA="Cats" optionB="Dogs"/>
+    <Vote optionA="Tabs" optionB="Spaces"/>
     <!-- <Vote optionA="Pizza" optionB="Hot Dogs"/> -->
   </div>
 </template>
